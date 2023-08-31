ef9e501135aa1139a85e54d9143d37007e18ed3d
ivana0905 <ivana.ilicic0905@gmail.com>
2023-08-31 21:46:48 +0200

    Add project files.

---
+++ ReviewApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReviewApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
